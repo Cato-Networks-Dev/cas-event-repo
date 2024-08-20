2898c3762c98bcdf9c8ae6bbaef09079763a7008
CAS User <162475337+Commerciallic@users.noreply.github.com>
2024-08-20 10:45:34 +0000

    Rollback

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
+#!/bin/bash
+
+file_to_check="my__test_file.txt"
+
+if [ -f "$file_to_check" ]; then
+  echo "$file_to_check exists."
+else
+  echo "$file_to_check does not exist."
+fi
